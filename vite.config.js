c61557b92ca3316002dbb1ebe8365d2ee1b5d784
Kazuya Goto <kaz.goto.jp@gmail.com>
2024-11-04 14:38:25 +0900

    add the basic music player

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+
+export default defineConfig((opt) => {
+    return {
+        root: "src",
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    api: "modern-compiler",
+                },
+            },
+        },
+        build: {
+            outDir: "../dist",
+            rollupOptions: {
+                input: {
+                    index: resolve(__dirname, "src/index.ts"),
+                },
+                output: {
+                    entryFileNames: "[name].js",
+                    assetFileNames: (assetInfo) => {
+                        if (assetInfo.name.endsWith(".mp3")) {
+                            return "assets/musics/[name][extname]";
+                        }
+                        return "assets/[name][extname]";
+                    },
+                },
+            },
+        },
+    };
+});
