ec241360db9d3c9f123701370d163c77581d5769
Kazuya Goto <kaz.goto.jp@gmail.com>
2024-11-07 22:40:02 +0900

    add: increment and decrement buttons for timer

--- src/parts/timer/timer.ts
+++ src/parts/timer/timer.ts
@@ -11,21 +11,88 @@ timer.addEventListener("mousedown", (e) => {
     e.stopPropagation();
 });
 
-const minutes = document.createElement("input");
-minutes.type = "number";
-minutes.value = "0";
-minutes.min = "0";
-minutes.max = "59";
+const minutesInput = document.createElement("input");
+minutesInput.type = "number";
+minutesInput.value = "0";
+minutesInput.min = "0";
+minutesInput.max = "59";
 
-const seconds = document.createElement("input");
-seconds.type = "number";
-seconds.value = "0";
-seconds.min = "0";
-seconds.max = "59";
+const minutesDisplay = document.createElement("span");
+minutesDisplay.className = "time-display";
+minutesDisplay.innerText = minutesInput.value.padStart(2, "0");
+minutesInput.addEventListener("change", () => {
+    minutesDisplay.innerText = minutesInput.value.padStart(2, "0");
+});
 
-const time = document.createElement("span");
-timer.appendChild(minutes);
-timer.appendChild(seconds);
+const incrementMinuteButton = document.createElement("button");
+incrementMinuteButton.innerText = "+";
+incrementMinuteButton.addEventListener("click", () => {
+    if (parseInt(minutesInput.value) >= 59) return;
+    minutesInput.value = (parseInt(minutesInput.value) + 1).toString();
+    minutesInput.dispatchEvent(new Event("change"));
+});
+
+const decrementMinuteButton = document.createElement("button");
+decrementMinuteButton.innerText = "-";
+decrementMinuteButton.addEventListener("click", () => {
+    if (parseInt(minutesInput.value) <= 0) return;
+    minutesInput.value = (parseInt(minutesInput.value) - 1).toString();
+    minutesInput.dispatchEvent(new Event("change"));
+});
+
+const minutesButtonWrapper = document.createElement("div");
+minutesButtonWrapper.className = "button-wrapper";
+minutesButtonWrapper.appendChild(incrementMinuteButton);
+minutesButtonWrapper.appendChild(decrementMinuteButton);
+
+const minutesWrapper = document.createElement("div");
+minutesWrapper.appendChild(minutesInput);
+minutesWrapper.appendChild(minutesDisplay);
+minutesWrapper.appendChild(minutesButtonWrapper);
+
+const secondsInput = document.createElement("input");
+secondsInput.type = "number";
+secondsInput.value = "0";
+secondsInput.min = "0";
+secondsInput.max = "59";
+
+const secondsDisplay = document.createElement("span");
+secondsDisplay.className = "time-display";
+secondsDisplay.innerText = secondsInput.value.padStart(2, "0");
+secondsInput.addEventListener("change", () => {
+    secondsDisplay.innerText = secondsInput.value.padStart(2, "0");
+});
+
+const incrementSecondButton = document.createElement("button");
+incrementSecondButton.innerText = "+";
+incrementSecondButton.addEventListener("click", () => {
+    if (parseInt(secondsInput.value) >= 59) return;
+    secondsInput.value = (parseInt(secondsInput.value) + 1).toString();
+    secondsInput.dispatchEvent(new Event("change"));
+});
+
+const decrementSecondButton = document.createElement("button");
+decrementSecondButton.innerText = "-";
+decrementSecondButton.addEventListener("click", () => {
+    if (parseInt(secondsInput.value) <= 0) return;
+    secondsInput.value = (parseInt(secondsInput.value) - 1).toString();
+    secondsInput.dispatchEvent(new Event("change"));
+});
+
+const secondsButtonWrapper = document.createElement("div");
+secondsButtonWrapper.className = "button-wrapper";
+secondsButtonWrapper.appendChild(incrementSecondButton);
+secondsButtonWrapper.appendChild(decrementSecondButton);
+
+const secondsWrapper = document.createElement("div");
+secondsWrapper.appendChild(secondsInput);
+secondsWrapper.appendChild(secondsDisplay);
+secondsWrapper.appendChild(secondsButtonWrapper);
+
+const timeWrapper = document.createElement("div");
+timeWrapper.className = "time-wrapper";
+timeWrapper.appendChild(minutesWrapper);
+timeWrapper.appendChild(secondsWrapper);
 
 let interval: number;
 let isRunning = false;
@@ -38,14 +105,16 @@ startButton.addEventListener("click", () => {
 
 function startTimer() {
     isRunning = true;
-    minutes.disabled = true;
-    seconds.disabled = true;
+    minutesInput.disabled = true;
+    secondsInput.disabled = true;
     startButton.innerText = "Stop";
-    let time = parseInt(minutes.value) * 60 + parseInt(seconds.value);
+    let time = parseInt(minutesInput.value) * 60 + parseInt(secondsInput.value);
     interval = setInterval(() => {
         time--;
-        minutes.value = Math.floor(time / 60).toString();
-        seconds.value = (time % 60).toString();
+        minutesInput.value = Math.floor(time / 60).toString();
+        minutesInput.dispatchEvent(new Event("change"));
+        secondsInput.value = (time % 60).toString();
+        secondsInput.dispatchEvent(new Event("change"));
         if (time <= 0) {
             se.play();
             stopTimer();
@@ -55,19 +124,21 @@ function startTimer() {
 
 function stopTimer() {
     isRunning = false;
-    minutes.disabled = false;
-    seconds.disabled = false;
+    minutesInput.disabled = false;
+    secondsInput.disabled = false;
     startButton.innerText = "Start";
     clearInterval(interval);
 }
 
-timer.appendChild(time);
+timer.appendChild(timeWrapper);
 timer.appendChild(startButton);
 
 function setTimer(min: number, sec: number) {
     if (!isRunning) {
-        minutes.value = min.toString();
-        seconds.value = sec.toString();
+        minutesInput.value = min.toString();
+        minutesInput.dispatchEvent(new Event("change"));
+        secondsInput.value = sec.toString();
+        secondsInput.dispatchEvent(new Event("change"));
     }
 }
 
