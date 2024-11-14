c96ebf19c0c64c0f23a19fb5ca0ac90677c414a0
Kazuya Goto <kaz.goto.jp@gmail.com>
2024-11-14 21:14:38 +0900

    refactor: index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,27 @@ import container from "./parts/container";
 import musicPlayer from "./parts/musicPlayer";
 import timer from "./parts/timer/timer";
 
-if (!(window as any)['onlineMeetingToolkitLoaded']) {
-    (window as any)['onlineMeetingToolkitLoaded'] = true;
-  
-    chrome.runtime.onMessage.addListener((message) => {
-      if (message.action === 'trigger') {
-        if (document.getElementById(container.id)) {
-            document.body.removeChild(container);
-        } else {
+function toggleDisplay() {
+    if (document.getElementById(container.id)) {
+        document.body.removeChild(container);
+    } else {
+        container.appendChild(timer);
+        container.appendChild(musicPlayer);
 
-            container.appendChild(timer);
-            container.appendChild(musicPlayer);
-
-            document.body.appendChild(container);
-        }
-      }
-    });
+        document.body.appendChild(container);
+    }
 }
+
+(function () {
+    if ((window as any)["onlineMeetingToolkitLoaded"]) {
+        return;
+    }
+
+    (window as any)["onlineMeetingToolkitLoaded"] = true;
+
+    chrome.runtime.onMessage.addListener((message) => {
+        if (message.action === "trigger") {
+            toggleDisplay();
+        }
+    });
+})();
