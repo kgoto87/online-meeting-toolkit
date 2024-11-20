28e1873e2f74023cb0bc836e01b5f14ca25431ad
Kazuya Goto <kaz.goto.jp@gmail.com>
2024-11-20 22:19:38 +0900

    refactor: predefined buttons to array

--- src/parts/timer/timer.ts
+++ src/parts/timer/timer.ts
@@ -11,22 +11,19 @@ const time = new Time();
 
 timer.appendChild(time.wrapper);
 
-const oneMunitueButton = createPredefinedButton(1, 0, () =>
-    time.setTimer(1, 0)
-);
-const fiveMunitueButton = createPredefinedButton(5, 0, () =>
-    time.setTimer(5, 0)
-);
-const tenMunitueButton = createPredefinedButton(10, 0, () =>
-    time.setTimer(10, 0)
-);
+const predefinedButtons = [
+    createPredefinedButton(1, 0, () => time.setTimer(1, 0)),
+    createPredefinedButton(5, 0, () => time.setTimer(5, 0)),
+    createPredefinedButton(10, 0, () => time.setTimer(10, 0)),
+];
 
 const predefinedButtonWrapper = document.createElement("div");
 predefinedButtonWrapper.className = "predefined-button-wrapper";
 
-predefinedButtonWrapper.appendChild(oneMunitueButton);
-predefinedButtonWrapper.appendChild(fiveMunitueButton);
-predefinedButtonWrapper.appendChild(tenMunitueButton);
+predefinedButtons.forEach((button) => {
+    predefinedButtonWrapper.appendChild(button);
+});
+
 timer.appendChild(predefinedButtonWrapper);
 
 export default timer;
