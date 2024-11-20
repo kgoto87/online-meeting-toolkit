00fa3b8f02bdf0a8e323e4b79366ff8de1fdac39
Kazuya Goto <kaz.goto.jp@gmail.com>
2024-11-20 22:16:41 +0900

    refactor: add time class

---
+++ src/parts/timer/time.ts
@@ -0,0 +1,34 @@
+import { state } from "../../state";
+import { createStartButton } from "./startButton";
+import { TimeUnit } from "./timeUnit";
+
+export class Time {
+    readonly minutes: TimeUnit;
+    readonly seconds: TimeUnit;
+    readonly wrapper: HTMLDivElement;
+
+    constructor() {
+        this.minutes = new TimeUnit();
+        this.seconds = new TimeUnit();
+
+        this.wrapper = document.createElement("div");
+        this.wrapper.className = "time-wrapper";
+        this.wrapper.appendChild(this.minutes.wrapper);
+        this.wrapper.appendChild(this.seconds.wrapper);
+
+        const startButton = createStartButton(
+            this.minutes.input,
+            this.seconds.input
+        );
+        this.wrapper.appendChild(startButton);
+    }
+
+    setTimer(min: number, sec: number) {
+        if (state.isRunning) return;
+
+        this.minutes.input.value = min.toString();
+        this.minutes.input.dispatchEvent(new Event("change"));
+        this.seconds.input.value = sec.toString();
+        this.seconds.input.dispatchEvent(new Event("change"));
+    }
+}
--- src/parts/timer/timer.ts
+++ src/parts/timer/timer.ts
@@ -1,7 +1,5 @@
 import createPredefinedButton from "./predefinedButton";
-import { createStartButton } from "./startButton";
-import { TimeUnit } from "./timeUnit";
-import { state } from "../../state";
+import { Time } from "./time";
 
 const timer = document.createElement("div");
 timer.className = "timer";
@@ -9,33 +7,19 @@ timer.addEventListener("mousedown", (e) => {
     e.stopPropagation();
 });
 
-const minutes = new TimeUnit();
-const seconds = new TimeUnit();
+const time = new Time();
 
-const timeWrapper = document.createElement("div");
-timeWrapper.className = "time-wrapper";
-timeWrapper.appendChild(minutes.wrapper);
-timeWrapper.appendChild(seconds.wrapper);
+timer.appendChild(time.wrapper);
 
-const startButton = createStartButton(minutes.input, seconds.input);
-
-timeWrapper.appendChild(startButton);
-timer.appendChild(timeWrapper);
-
-function setTimer(min: number, sec: number) {
-    if (state.isRunning) {
-        return;
-    }
-
-    minutes.input.value = min.toString();
-    minutes.input.dispatchEvent(new Event("change"));
-    seconds.input.value = sec.toString();
-    seconds.input.dispatchEvent(new Event("change"));
-}
-
-const oneMunitueButton = createPredefinedButton(1, 0, () => setTimer(1, 0));
-const fiveMunitueButton = createPredefinedButton(5, 0, () => setTimer(5, 0));
-const tenMunitueButton = createPredefinedButton(10, 0, () => setTimer(10, 0));
+const oneMunitueButton = createPredefinedButton(1, 0, () =>
+    time.setTimer(1, 0)
+);
+const fiveMunitueButton = createPredefinedButton(5, 0, () =>
+    time.setTimer(5, 0)
+);
+const tenMunitueButton = createPredefinedButton(10, 0, () =>
+    time.setTimer(10, 0)
+);
 
 const predefinedButtonWrapper = document.createElement("div");
 predefinedButtonWrapper.className = "predefined-button-wrapper";
