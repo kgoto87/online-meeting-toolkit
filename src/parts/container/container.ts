4c0bfeaf1abddd3fbdb5956beea483524861dd94
Kazuya Goto <kaz.goto.jp@gmail.com>
2024-11-22 05:57:51 +0900

    WIP: refactoring mouse event handlers

--- src/parts/container/container.ts
+++ src/parts/container/container.ts
@@ -1,10 +1,11 @@
-import { MouseEvents } from "./mouseEvents";
+import { mover } from "./mouseEvents";
 
 const container = document.createElement("div");
 container.id = "online-meeting-toolkit";
 container.style.bottom = "0";
 container.style.right = "0";
 
-new MouseEvents(container);
+//new MouseEvents(container);
+container.addEventListener("mousedown", mover.onMouseDown);
 
 export default container;
