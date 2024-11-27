36c713317bac57f10b4e2183113f4507dfa42188
Kazuya Goto <kaz.goto.jp@gmail.com>
2024-11-28 05:57:18 +0900

    test: add top and left edge tests

--- src/parts/container/events/mouseEvents.test.ts
+++ src/parts/container/events/mouseEvents.test.ts
@@ -1,10 +1,12 @@
 import { beforeEach, expect, test } from "vitest";
 import { MouseEvents } from "./mouseEvents";
 
-const mockTarget = document.createElement("div");
-new MouseEvents(mockTarget);
+let mockTarget: HTMLElement;
 
 beforeEach(function () {
+    window = Object.assign(window, { innerWidth: 100 });
+    mockTarget = document.createElement("div");
+    new MouseEvents(mockTarget);
     document.dispatchEvent(new MouseEvent("mouseup"));
 });
 
@@ -44,3 +46,28 @@ test("should not move target when not dragging", function () {
     expect(mockTarget.style.left).toBe("10px");
     expect(mockTarget.style.top).toBe("10px");
 });
+
+test("should stop at left edge", function () {
+    mockTarget.dispatchEvent(
+        new MouseEvent("mousedown", { clientX: 0, clientY: 0 })
+    );
+    // do not really want to simulate the offsetLeft in this way but nothing else worked
+    Object.defineProperty(mockTarget, "offsetLeft", { get: () => -10 });
+    document.dispatchEvent(
+        new MouseEvent("mousemove", { clientX: -10, clientY: 0 })
+    );
+    expect(mockTarget.style.left).toBe("0px");
+});
+
+test("should stop at top edge", function () {
+    mockTarget.dispatchEvent(
+        new MouseEvent("mousedown", { clientX: 0, clientY: 0 })
+    );
+    Object.defineProperty(mockTarget, "offsetTop", { get: () => -10 });
+    document.dispatchEvent(
+        new MouseEvent("mousemove", { clientX: 0, clientY: -10 })
+    );
+    expect(mockTarget.style.top).toBe("0px");
+});
+
+// NOTE: not testing the right and bottom edges as styles cannot be oveerwritten in jsdom
