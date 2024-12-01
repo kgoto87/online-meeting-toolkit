12efbce97e09eacf26cedebe218b3eb767d0010e
Kazuya Goto <kaz.goto.jp@gmail.com>
2024-12-01 21:34:12 +0900

    style: move position to css

--- src/parts/container/container.ts
+++ src/parts/container/container.ts
@@ -3,8 +3,6 @@ import { WindowEvents } from "./events/windowEvents";
 
 const container = document.createElement("div");
 container.id = "online-meeting-toolkit";
-container.style.bottom = "0";
-container.style.right = "0";
 
 new MouseEvents(container);
 new WindowEvents(container);
