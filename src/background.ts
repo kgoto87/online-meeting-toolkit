c4f69118831c824746a730b01d5c5c0789bd14e5
Kazuya Goto <kaz.goto.jp@gmail.com>
2024-12-05 05:52:19 +0900

    style: auto lint

--- src/background.ts
+++ src/background.ts
@@ -1,27 +1,27 @@
 chrome.action.onClicked.addListener(async (tab) => {
     try {
-        const [{result}] = await chrome.scripting.executeScript({
-            target: {tabId: tab.id!},
-            func: () => (window as any)['onlineMeetingToolkitLoaded']
+        const [{ result }] = await chrome.scripting.executeScript({
+            target: { tabId: tab.id! },
+            func: () => (window as any)["onlineMeetingToolkitLoaded"],
         });
 
         if (result) {
-            chrome.tabs.sendMessage(tab.id!, { action: 'trigger' });
+            chrome.tabs.sendMessage(tab.id!, { action: "trigger" });
             return;
-          }
+        }
     } catch {
         // Script not loaded yet, proceed with injection
     }
 
     await chrome.scripting.insertCSS({
-        target: {tabId: tab.id!},
-        files: ['assets/index.css']
+        target: { tabId: tab.id! },
+        files: ["assets/index.css"],
     });
 
     await chrome.scripting.executeScript({
-        target: {tabId: tab.id!},
-        files: ['index.js']
+        target: { tabId: tab.id! },
+        files: ["index.js"],
     });
 
-    chrome.tabs.sendMessage(tab.id!, { action: 'trigger' });
+    chrome.tabs.sendMessage(tab.id!, { action: "trigger" });
 });
