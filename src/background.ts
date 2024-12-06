bfe3032c6fbe0e394a213c04a8b5078adbdb4c3d
Kazuya Goto <kaz.goto.jp@gmail.com>
2024-12-07 05:53:56 +0900

    feat: a flag to always display the toolkit

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,27 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.set({ onNewTab: false });
+});
+
+chrome.tabs.onActivated.addListener(async ({ tabId }) => {
+    const tab = await chrome.tabs.get(tabId!);
+
+    chrome.storage.sync.get("onNewTab", async (items) => {
+        if (items.onNewTab) {
+            await chrome.scripting.insertCSS({
+                target: { tabId: tab.id! },
+                files: ["assets/index.css"],
+            });
+
+            await chrome.scripting.executeScript({
+                target: { tabId: tab.id! },
+                files: ["index.js"],
+            });
+
+            chrome.tabs.sendMessage(tab.id!, { action: "trigger" });
+        }
+    });
+});
+
 chrome.action.onClicked.addListener(async (tab) => {
     try {
         const [{ result }] = await chrome.scripting.executeScript({
