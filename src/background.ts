86f63111840eacf1e33c52857f127f18e33a8413
Kazuya Goto <kaz.goto.jp@gmail.com>
2024-12-08 06:06:30 +0900

    feat: always display the toolkit on reload

--- src/background.ts
+++ src/background.ts
@@ -1,51 +1,83 @@
+type storageItems = {
+    onNewTab: boolean;
+};
+
+async function alreadyLoaded(tabId: number): Promise<boolean> {
+    try {
+        const [{ result }] = await chrome.scripting.executeScript({
+            target: { tabId },
+            func: () => (window as any)["onlineMeetingToolkitLoaded"],
+        });
+
+        return !!result;
+    } catch {
+        return false;
+    }
+}
+
+async function loadToolkit(tabId: number) {
+    await chrome.scripting.insertCSS({
+        target: { tabId },
+        files: ["assets/index.css"],
+    });
+
+    await chrome.scripting.executeScript({
+        target: { tabId },
+        files: ["index.js"],
+    });
+
+    chrome.tabs.sendMessage(tabId, { action: "trigger" });
+}
+
+async function executeOnNewTab(tabId: number, items: storageItems) {
+    if (await alreadyLoaded(tabId)) return;
+
+    try {
+        if (items.onNewTab) {
+            await loadToolkit(tabId);
+        }
+    } catch (e) {
+        if (!(e instanceof Error)) return;
+
+        if (
+            e.message.includes("Cannot access a chrome:// URL") ||
+            e.message.includes("Cannot access contents of the page")
+        ) {
+            return;
+        }
+
+        console.info(e);
+    }
+}
+
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ onNewTab: false });
+});
+
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+    if (changeInfo.status === "complete") {
+        if (tab.url && tab.url.includes("chrome://")) return;
+
+        chrome.storage.sync.get("onNewTab", async (items) =>
+            executeOnNewTab(tabId, items as storageItems)
+        );
+    }
 });
 
 chrome.tabs.onActivated.addListener(async ({ tabId }) => {
     const tab = await chrome.tabs.get(tabId!);
 
-    chrome.storage.sync.get("onNewTab", async (items) => {
-        if (items.onNewTab) {
-            await chrome.scripting.insertCSS({
-                target: { tabId: tab.id! },
-                files: ["assets/index.css"],
-            });
+    if (tab.url && tab.url.includes("chrome://")) return;
 
-            await chrome.scripting.executeScript({
-                target: { tabId: tab.id! },
-                files: ["index.js"],
-            });
-
-            chrome.tabs.sendMessage(tab.id!, { action: "trigger" });
-        }
-    });
+    chrome.storage.sync.get("onNewTab", async (items) =>
+        executeOnNewTab(tabId, items as storageItems)
+    );
 });
 
 chrome.action.onClicked.addListener(async (tab) => {
-    try {
-        const [{ result }] = await chrome.scripting.executeScript({
-            target: { tabId: tab.id! },
-            func: () => (window as any)["onlineMeetingToolkitLoaded"],
-        });
-
-        if (result) {
-            chrome.tabs.sendMessage(tab.id!, { action: "trigger" });
-            return;
-        }
-    } catch {
-        // Script not loaded yet, proceed with injection
+    if (await alreadyLoaded(tab.id!)) {
+        chrome.tabs.sendMessage(tab.id!, { action: "trigger" });
+        return;
     }
-
-    await chrome.scripting.insertCSS({
-        target: { tabId: tab.id! },
-        files: ["assets/index.css"],
-    });
-
-    await chrome.scripting.executeScript({
-        target: { tabId: tab.id! },
-        files: ["index.js"],
-    });
-
-    chrome.tabs.sendMessage(tab.id!, { action: "trigger" });
+    await loadToolkit(tab.id!);
 });
