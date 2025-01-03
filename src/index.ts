02c77669729b92d5e12c04164e5ccc3a68923599
Kazuya Goto <kaz.goto.jp@gmail.com>
2025-01-03 21:34:01 +0900

    refactor: change scss file name

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import "./styles/styles.scss";
+import "./styles/index.scss";
 import container from "./parts/container/container";
 import musicPlayer from "./parts/musicPlayer";
 import timer from "./parts/timer/timer";
