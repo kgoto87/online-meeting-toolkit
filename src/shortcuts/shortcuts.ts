46b77550a4588a5a104a12a43f847f75475a5841
Kazuya Goto <kaz.goto.jp@gmail.com>
2025-01-16 05:51:53 +0900

    wip: refactor shortcuts

--- src/shortcuts/shortcuts.ts
+++ src/shortcuts/shortcuts.ts
@@ -2,15 +2,31 @@ import { togglePlay } from "../parts/musicPlayer/musicPlayer";
 
 const pressedKeys = new Set<string>();
 
+type Key = "a" | "s" | "d" | "z" | "x" | "c" | "v";
+
+class KeyboardShortcutEvaluator {
+    private hasBasicKeys: boolean;
+
+    constructor(private e: KeyboardEvent) {
+        this.hasBasicKeys = e.ctrlKey && e.shiftKey;
+    }
+
+    has(key: Key): boolean {
+        return (
+            this.hasBasicKeys &&
+            this.e.key.toLowerCase() === key &&
+            pressedKeys.size === 3
+        );
+    }
+}
+
 document.addEventListener("keydown", (e: KeyboardEvent) => {
+    // TODO: need to move this to the KeyboardShortcutEvaluator
     pressedKeys.add(e.code);
 
-    if (
-        e.ctrlKey &&
-        e.shiftKey &&
-        e.key.toLowerCase() === "a" &&
-        pressedKeys.size === 3
-    ) {
+    const evaluator = new KeyboardShortcutEvaluator(e);
+
+    if (evaluator.has("a")) {
         e.preventDefault();
         togglePlay();
     }
