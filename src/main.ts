7e5a91234b47d772bf98ca71110a22d8ac57e2d0
Kazuya Goto <kaz.goto.jp@gmail.com>
2025-01-16 22:04:30 +0900

    refactor: container module path

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import "./styles/main.scss";
-import container from "./parts/container/container";
+import container from "./parts/container";
 import musicPlayer from "./parts/musicPlayer";
 import timer from "./parts/timer";
 
