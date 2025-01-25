ab5eb69f534720c4f45840a1a297c8b43c9a317d
Kazuya Goto <kaz.goto.jp@gmail.com>
2025-01-25 21:16:04 +0900

    feat: set sound effect volume

--- src/parts/timer/soundEffect.ts
+++ src/parts/timer/soundEffect.ts
@@ -2,6 +2,17 @@ import gong from "../../public/assets/musics/gong.mp3";
 
 const se = document.createElement("audio");
 se.src = chrome.runtime.getURL(gong);
-se.volume = 0.02;
+chrome.storage.sync.get("soundVolume", (items) => {
+    changeVolumeTo(items.soundVolume);
+});
+
+chrome.storage.onChanged.addListener((changes) => {
+    if (changes.soundVolume === undefined) return;
+    changeVolumeTo(changes.soundVolume.newValue);
+});
+
+function changeVolumeTo(volume: number) {
+    se.volume = volume / 100;
+}
 
 export default se;
