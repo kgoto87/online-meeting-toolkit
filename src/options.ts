9f4104f7882b425ae368b7512439cb2adfad038a
Kazuya Goto <kaz.goto.jp@gmail.com>
2025-01-25 20:52:10 +0900

    feat: store the sound volume to storage

--- src/options.ts
+++ src/options.ts
@@ -3,12 +3,20 @@ import "./styles/options.scss";
 const messagingTime = 2000;
 const messagingBuffer = messagingTime + 100;
 const onNewTabInput = document.getElementById("on-new-tab") as HTMLInputElement;
+const soundVolumeInput = document.getElementById(
+    "sound-volume"
+) as HTMLInputElement;
+const defaultSoundVolume = 2;
 const message = document.getElementById("message") as HTMLDivElement;
 const messageText = document.createElement("p");
 
 const saveOptions = (e: Event) => {
     e.preventDefault();
-    chrome.storage.sync.set({ onNewTab: onNewTabInput.checked }, () => {
+    const options = {
+        onNewTab: onNewTabInput.checked,
+        soundVolume: soundVolumeInput.value,
+    };
+    chrome.storage.sync.set(options, () => {
         messageText.textContent = "Options saved.";
         message.appendChild(messageText);
         message.classList.add("show");
@@ -17,16 +25,23 @@ const saveOptions = (e: Event) => {
         }, messagingTime);
         setTimeout(() => {
             message.textContent = "";
-            message.removeChild(messageText);
+            if (message.hasChildNodes()) {
+                message.removeChild(messageText);
+            }
         }, messagingBuffer);
     });
 };
 
 const restoreOptions = () => {
-    chrome.storage.sync.get({ onNewTab: false }, (items) => {
-        onNewTabInput.checked = items.onNewTab;
-    });
+    chrome.storage.sync.get(
+        { onNewTab: false, soundVolume: defaultSoundVolume },
+        (items) => {
+            onNewTabInput.checked = items.onNewTab;
+            soundVolumeInput.value = items.soundVolume;
+        }
+    );
 };
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
 onNewTabInput.addEventListener("change", saveOptions);
+soundVolumeInput.addEventListener("input", saveOptions);
