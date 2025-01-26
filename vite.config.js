b25beaed3753ec9d5a44c31f2ae1c83a9928394b
Kazuya Goto <kaz.goto.jp@gmail.com>
2025-01-26 21:53:08 +0900

    refactor: all options files in one directory

--- vite.config.js
+++ vite.config.js
@@ -18,15 +18,25 @@ export default defineConfig((opt) => {
                     main: resolve(__dirname, "src/main.ts"),
                     background: resolve(__dirname, "src/background.ts"),
                     options: resolve(__dirname, "src/options/index.ts"),
+                    optionsHtml: resolve(__dirname, "src/options/index.html"),
                 },
                 output: {
-                    entryFileNames: "[name].js",
+                    entryFileNames: (entryInfo) => {
+                        let path = "[name].js";
+                        if (entryInfo.name === "options") {
+                            path = "options/index.js";
+                        }
+                        return path;
+                    },
                     assetFileNames: (assetInfo) => {
                         let path = "assets/[name][extname]";
                         if (assetInfo.names) {
                             assetInfo.names.forEach((name) => {
                                 if (name.endsWith(".mp3")) {
                                     path = "assets/musics/[name][extname]";
+                                }
+                                if (name === "options.css") {
+                                    path = "options/options.css";
                                 }
                             });
                         }
